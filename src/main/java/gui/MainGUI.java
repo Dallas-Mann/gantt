72f7e39c0219641da72dbc74de99a95e47a53fc0
myuser <myuser@localhost.localdomain>
2019-11-23 18:48:31 -0700

    Initial commit working hellow world java fx gui working gradle built script

---
+++ src/main/java/gui/MainGUI.java
@@ -0,0 +1,26 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainGUI extends Application
+{
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.sizeToScene();
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args)
+    {
+        Application.launch(args);
+    }
+}
